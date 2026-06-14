9fc8001e0e67734ae4690f86b0280e69ab0209a1
Andrew <kad.io.work@gmail.com>
2026-06-14 17:43:26 +0300

    Add existing project files to Git

---
+++ calculator.py
@@ -0,0 +1,21 @@
+
+class Calc:
+    @staticmethod
+    def multiplies(a, b):
+        return a*b
+
+    @staticmethod
+    def divides(a, b):
+        return a/b
+
+    @staticmethod
+    def adds(a, b):
+        return a+b
+
+    @staticmethod
+    def subtracts(a, b):
+        return a-b
+
+    @staticmethod
+    def exponentiates(a, b):
+        return pow(a, b)
