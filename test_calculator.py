a77a8ec97f1a42cbd925e88238fc801fe469236f
Andrew <kad.io.work@gmail.com>
2026-06-14 18:16:32 +0300

    Add existing project files to Git

--- test_calculator.py
+++ test_calculator.py
@@ -3,13 +3,13 @@ import pytest
 from calculator import Calc
 
 
-@pytest.mark.calc
+@pytest.mark.unittests
 @pytest.mark.add
 def test_add_calc():
     assert Calc.adds(2, 3) == 5
 
 
-@pytest.mark.calc
+@pytest.mark.unittests
 @pytest.mark.div
 def test_div_calc():
     assert Calc.divides(4, 2) == 2
